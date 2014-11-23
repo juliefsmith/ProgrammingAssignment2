66bd4fc3d24f8dd69a65dd292fff23030e954d91
Julie Smith <juliefsmith@gmail.com>
2014-11-23 14:23:08 -0800

    Adding comments
    Comments added to explain goal of functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file consists of a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## This function creates a matrix object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the matrix returned by makeCacheMatrix. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
